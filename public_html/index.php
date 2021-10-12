ad0c1f5c4fe5442ed9887d8fe72b7b58f37c491f
RamirezJosue <xdemobtkx@gmail.com>
2021-10-12 11:25:45 -0500

    update

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,30 @@
-<h1>Hello World!!!</h1>
+<h1>Hello Cloudreach!</h1>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
+<h4>Attempting MySQL connection from php...</h4>
 <h4>Attempting MySQL connection from php...</h4>
 <?php
-// I assume that you've created a new username and password
 $host = 'mysql';
-$user = 'user';
-$pass = 'user';
+$user = 'root';
+$pass = 'rootpassword';
 $conn = new mysqli($host, $user, $pass);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-  echo "Connected to MySQL successfully!";
+    echo "Connected to MySQL successfully!";
 }
+
 ?>
