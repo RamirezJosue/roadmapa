f9d6f3614d65e46baf3ec515e4353e381affb5db
RamirezJosue <xdemobtkx@gmail.com>
2021-10-12 14:40:58 -0500

    update

--- public_html/index.php
+++ public_html/index.php
@@ -1,35 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Hello there</title>
-        <style>
-            body {
-                font-family: "Arial", sans-serif;
-                font-size: larger;
-            }
+<h1>Hello Cloudreach!</h1>
+<h4>Attempting MySQL connection from php...</h4>
+<?php
+$host = 'mysql';
+$user = 'root';
+$pass = 'rootpassword';
+$conn = new mysqli($host, $user, $pass);
 
-            .center {
-                display: block;
-                margin-left: auto;
-                margin-right: auto;
-                width: 50%;
-            }
-        </style>
-    </head>
-    <body>
-        <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
-        <?php
-            $host = 'mysql';
-            $user = 'root';
-            $pass = 'rootpassword';
-            $conn = new mysqli($host, $user, $pass);
-            
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            } else {
-                echo "Connected to MySQL successfully!";
-            }
-        ?>
-    </body>
-</html>
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL successfully!";
+}
+
+?>
