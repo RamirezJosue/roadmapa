85d000708343affd4cd6eb8f14ddbfd006b51c26
RamirezJosue <xdemobtkx@gmail.com>
2021-10-12 14:39:26 -0500

    update

--- public_html/index.php
+++ public_html/index.php
@@ -20,20 +20,16 @@
     <body>
         <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
         <?php
-        $connection = new PDO('mysql:host=mysql;dbname=roadmap;charset=utf8', 'root', 'rootpassword');
-        $query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'usuarios'");
-        $tables     = $query->fetchAll(PDO::FETCH_COLUMN);
-
-        if (empty($tables)) {
-            echo '<p class="center">There are no tables in database <code>demo</code>.</p>';
-        } else {
-            echo '<p class="center">Database <code>demo</code> contains the following tables:</p>';
-            echo '<ul class="center">';
-            foreach ($tables as $table) {
-                echo "<li>{$table}</li>";
+            $host = 'mysql';
+            $user = 'root';
+            $pass = 'rootpassword';
+            $conn = new mysqli($host, $user, $pass);
+            
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            } else {
+                echo "Connected to MySQL successfully!";
             }
-            echo '</ul>';
-        }
         ?>
     </body>
 </html>
